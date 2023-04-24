24b9ae63117fc764615d95a9da3618c2e77f38ea
tommyming <tommyhonym@gmail.com>
2023-04-24 23:57:47 +0800

    Add Customer backend routes

--- ktor-create-htp-api/src/main/kotlin/com/example/plugins/Routing.kt
+++ ktor-create-htp-api/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.routes.customerRouting
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -9,5 +10,7 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+
+        customerRouting()
     }
 }
